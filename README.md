4379b43e45ffd486cc6be948cbd5d0840bbdb3af
beggsfinest00 <81796822+beggsfinest00@users.noreply.github.com>
2023-02-27 14:15:58 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Variables_Dallas
+ 
---
+++ Variables_Dallas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Variables_Dallas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declars variables
+            int favoriteNumber;
+            bool isJumping = false, isRunning = false;
+            float myFloat;
+            favoriteNumber = 6;
+            myFloat = 54.65f;
+            const double finalGrade = 90.0;
+            // displays variables
+            Console.WriteLine("favorite #: " + favoriteNumber);
+            Console.WriteLine("jumping: " + isJumping);
+            Console.WriteLine("running: " + isRunning);
+            Console.WriteLine("Random Floating #: " + myFloat);
+            Console.WriteLine("Final Grade #: " + finalGrade);
+
+        }
+    }
+}
